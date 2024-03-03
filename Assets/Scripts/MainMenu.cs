44a72ec87f7d139d154e046fca2ac3357a682dcc
Psevd0nim <megabrain451@gmail.com>
2024-03-03 21:52:45 +0500

    Tittle 0.5
    Play Button, Settings Button and something else

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,9 +4,27 @@ using System.Collections.Generic;
 using UnityEditor;
 #endif
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject MainTittle;    
+    
+    public void SettingsButton()
+    {
+        MainTittle.SetActive(false);
+    }
+
+    public void SettingsOff()
+    {
+        MainTittle.SetActive(true);
+    }
+    
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
     public void ExitButton()
     {
 #if UNITY_EDITOR
