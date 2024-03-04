26d9d200b7d293f9aae475389b4d8731a05fbbf7
Psevd0nim <megabrain451@gmail.com>
2024-03-04 17:59:05 +0500

    Tittle 0.9
    Almost made tittle and started main scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,23 +1,109 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject MainTittle;    
+    [SerializeField] private GameObject MainTittle;
+    [SerializeField] private GameObject Settings;
+    [SerializeField] private Button JumpHotKeyButton;
+    private KeyCode JumpHotKey;
+    [SerializeField] private GameObject PressAnyKeyText;
+    private TextMeshProUGUI JumpHotKeyText;
+    private bool JumpHotKeyPressed;
+
+    private void Awake()
+    {
+        UploadData();
+    }
+
+    private void Start()
+    {
+        JumpHotKeyButton.onClick.AddListener(() => HotKeyChangePressed());
+        JumpHotKeyText = JumpHotKeyButton.GetComponentInChildren<TextMeshProUGUI>();
+        JumpHotKeyText.text = JumpHotKey.ToString();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && Settings.activeInHierarchy)
+        {
+            SettingsOff();
+        }
+        if(JumpHotKeyPressed)
+        {
+            HotKeyChanging();
+        }
+
+    }
+
+    private void HotKeyChanging()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(keyCode))
+                {
+                    JumpHotKey = keyCode;
+                    JumpHotKeyText.text = keyCode.ToString();
+                    JumpHotKeyPressed = false;
+                    PressAnyKeyText.SetActive(false);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void HotKeyChangePressed()
+    {
+        PressAnyKeyText.SetActive(true);
+        JumpHotKeyPressed = true;
+    }
     
     public void SettingsButton()
     {
         MainTittle.SetActive(false);
+        Settings.SetActive(true);
     }
 
     public void SettingsOff()
     {
         MainTittle.SetActive(true);
+        Settings.SetActive(false);
+        SaveData();
+    }
+
+    void SaveData()
+    {
+        var data = new SettingsData { JumpKey = JumpHotKey };
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/datasave.json", json);
+    }
+
+    void UploadData()
+    {
+        string path = Application.persistentDataPath + "/datasave.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SettingsData data = JsonUtility.FromJson<SettingsData>(json);
+            JumpHotKey = data.JumpKey;
+        }
+        else JumpHotKey = KeyCode.Space;
+    }
+
+    [Serializable] class SettingsData
+    {
+        public KeyCode JumpKey;
     }
     
     public void PlayButton()
