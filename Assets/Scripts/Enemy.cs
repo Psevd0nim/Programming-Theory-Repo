11014b3f1b69b689e819338d067872af35dbf2de
Psevd0nim <megabrain451@gmail.com>
2024-03-04 23:20:36 +0500

    003
    Created enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private int countStep;
+    private Vector3 moveDirection = Vector3.left;
+    private SpriteRenderer spriteRenderer;
+    private bool flipBool = true;
+
+    private void Start()
     {
-        
+        StartCoroutine(Move(moveDirection));
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void MoveStep(Vector3 direction)
     {
-        
+        gameObject.transform.Translate(direction * 2);
+        countStep++;
+    }
+
+    IEnumerator Move(Vector3 direction)
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log(countStep);
+        MoveStep(direction);
+        if (countStep == 5)
+        {
+            spriteRenderer.flipX = !flipBool;
+            flipBool = !flipBool;
+            countStep = 0;
+            direction.x *= -1;
+        }
+        StartCoroutine(Move(direction));
     }
 }
